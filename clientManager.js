22626f69a8fcd3818169ae8706e93729a361f538
seoshi1234 <seoshi999@gmail.com>
2022-08-31 17:41:54 +0900

    first commit

---
+++ clientManager.js
@@ -0,0 +1,82 @@
+const dgram = require('dgram');
+
+
+//@ts-check
+
+class ClientManager{
+
+  constructor(server){
+    /** @type {Array<{address:string, port:string, time:Date, id:string}>} */
+    this.clients=[];
+    /** @type {dgram.Socket} */
+    this.server = server;
+    setInterval(this.checkHeartbeat.bind(this), 1000);
+  }
+
+  /**
+   * add new client or store heartbeat
+   * send back uid to client
+   * @param {string} address
+   * @param {string} port
+   */
+  pushClient(address, port){
+    
+    const idx = this.clients.findIndex(client=>client.address === address && client.port === port)    
+    if(idx != -1) {
+      this.clients[idx].time = new Date();
+      return;
+    }
+    const uid = this.generateRandomToken();
+    this.clients.push({address:address, port:port, time:new Date(), id:uid});
+    
+    const msg = new Buffer.from(`UID:${uid}`)
+    this.server.send(msg, 0, msg.length, port, address, ()=>{
+      console.log(`uid was sent to user ${uid}`);
+    });
+  }
+
+  /**
+   * check and delete disconnected client
+   */
+  checkHeartbeat(){
+
+    if(this.clients.length>0){
+      console.log(this.clients);      
+    }
+    
+    this.clients = this.clients.filter(client=>{
+      const diff = new Date().getTime() - client.time.getTime();
+      if(diff <= 3000) return true;
+      console.log(`User ${client.id} was disconnected.`);
+      return false;
+    })
+  }
+
+  s4(){
+    return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+  }
+
+  generateRandomToken(){
+    return `${this.s4()}-${this.s4()}`;
+  }
+
+  /**
+   * send to specific client
+   * @param {string} uid 
+   * @param {string} value
+   */
+  sendTo(uid, value){
+    const uidToLower = uid.toLowerCase();
+    this.clients.forEach(client=>{
+      if(client.id === uidToLower){
+        const msg = new Buffer.from(`Controller:${value}`)
+        this.server.send(msg, 0, msg.length, client.port, client.address, null);
+      }
+    })
+  }
+
+}
+
+module.exports = ClientManager;
---
+++ index.js
@@ -0,0 +1,32 @@
+const dgram = require('dgram');
+const ClientManager = require('./clientManager');
+
+
+const server = dgram.createSocket('udp4');
+const clientManager = new ClientManager(server);
+
+server.on('error', (err) => {
+  console.log(`server error:\n${err.stack}`);
+  server.close();
+});
+
+server.on('message', (msg, senderInfo) => {
+  const params = msg.toString().split(' ');
+  if(msg == 'Heartbeat'){
+    clientManager.pushClient(senderInfo.address, senderInfo.port);
+    return;
+  }
+  switch(params[0]){
+    case 'Controller':
+      clientManager.sendTo(params[1], params.slice(2).join(' '));
+      break;
+  }
+
+});
+
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening on ${address.address}:${address.port}`);
+});
+
+server.bind(5500, '127.0.0.1');
